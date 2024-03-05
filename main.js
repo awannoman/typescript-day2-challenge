63e88e02f58ae6456efecb6f51da2ef1f6855128
awannoman <160035058+awannoman@users.noreply.github.com>
2024-03-05 11:12:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+// // // // Day2
+//  Q4
+console.log('Albert Einstein once said,"A person who never take made a mistake never tried anything new"');
+//  Q5
+let famous_PersonName = "Albert Einstein";
+let message = ('once said,"A person who never take made a mistake never tried anything new"');
+console.log(famous_PersonName, message);
+// Q6
+let Name = "\t\n Noman \t\n";
+console.log(Name);
+let Name2 = Name.trim();
+console.log(Name2);
